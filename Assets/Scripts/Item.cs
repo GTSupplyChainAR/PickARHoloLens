0f618d23c0961070fb9ba3d64471668e49c16735
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2017-01-16 15:20:57 -0500

    Added initial warehouse scene

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace PickAR {
+
+    /// <summary>
+    /// An item that is part of the warehouse inventory.
+    /// </summary>
+    class Item : MonoBehaviour {
+
+        /// <summary> The aisle number of the item. </summary>
+        [SerializeField]
+        [Tooltip("The aisle number of the item.")]
+        private string aisleNumber;
+    }
+}
---
+++ Assets/Scripts/Managers/JobManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace PickAR.Managers {
+    /// <summary>
+    /// Keeps track of the user's current order picking job.
+    /// </summary>
+    class JobManager : MonoBehaviour {
+        
+        /// <summary> The items targeted by the user. </summary>
+        [SerializeField]
+        [Tooltip("The items targeted by the user.")]
+        private Item[] targetItems;
+
+        /// <summary>
+        /// Initializes the object.
+        /// </summary>
+        private void Start() {
+            
+        }
+
+        /// <summary>
+        /// Updates the object.
+        /// </summary>
+        private void Update() {
+            
+        }
+    }
+}
